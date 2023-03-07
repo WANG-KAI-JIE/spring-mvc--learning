cf50758443622115743c316fba4ccd8d1dfa968a
kjwang <1729298243@qq.com>
2023-03-07 16:08:22 +0800

    Spring MVC 数据绑定 学习

--- src/main/java/top/kjwang/entity/Person.java
+++ src/main/java/top/kjwang/entity/Person.java
@@ -9,6 +9,6 @@ import java.util.Date;
 public class Person {
     private String name;
     private Integer age;
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone ="GMT+8")
+//    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone ="GMT+8")
     private Date birthDay;
 }

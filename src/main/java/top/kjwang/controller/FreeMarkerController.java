4571d60e737bfc832ac0571f8ca0d033cb40f268
kjwang <1729298243@qq.com>
2023-03-08 20:59:49 +0800

    Spring MVC 数据绑定 学习

--- src/main/java/top/kjwang/controller/FreeMarkerController.java
+++ src/main/java/top/kjwang/controller/FreeMarkerController.java
@@ -1,10 +1,15 @@
 package top.kjwang.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import top.kjwang.convert.E;
 import top.kjwang.entity.User;
+
+import java.util.ArrayList;
 
 /**
  * @author kjwang
@@ -13,13 +18,57 @@ import top.kjwang.entity.User;
 
 @Controller
 @RequestMapping("/fm")
-public class FreeMarkerController {
-    @GetMapping("/test")
-    public ModelAndView showTest(){
-        ModelAndView mav = new ModelAndView("/test");
-        User user = new User();
-        user.setUsername("tom");
-        mav.addObject("u",user);
-        return mav;
+public class FreeMarkerController{
+    @RequestMapping("/test")
+    @ResponseBody
+    public String showTest(Model model){
+        ArrayList<Object> lists = new ArrayList<>();
+        {
+            E e1 = E.builder()
+                    .url("https://spring.io/img/icons/microservices.svg")
+                    .name("Microservices")
+                    .content("Quickly deliver production‑grade features with independently evolvable microservices.")
+                    .build();
+            E e2 = E.builder()
+                    .url("https://spring.io/img/icons/reactive.svg")
+                    .name("Reactive")
+                    .content("Spring's asynchronous, nonblocking architecture means you can get more from your computing resources.")
+                    .build();
+            E e3 = E.builder()
+                    .url("https://spring.io/img/icons/cloud.svg")
+                    .name("Cloud")
+                    .content("Your code, any cloud—we’ve got you covered. Connect and scale your services, whatever your platform.")
+                    .build();
+            E e4 = E.builder()
+                    .url("https://spring.io/img/icons/web-apps.svg")
+                    .name("Web apps")
+                    .content("Frameworks for fast, secure, and responsive web applications connected to any data store.")
+                    .build();
+            E e5 = E.builder()
+                    .url("https://spring.io/img/icons/serverless.svg")
+                    .name("Serverless")
+                    .content("The ultimate flexibility. Scale up on demand and scale to zero when there’s no demand.")
+                    .build();
+            E e6 = E.builder()
+                    .url("https://spring.io/img/icons/event-driven.svg")
+                    .name("Event Driven")
+                    .content("Integrate with your enterprise. React to business events. Act on your streaming data in realtime.")
+                    .build();
+            E e7 = E.builder()
+                    .url("https://spring.io/img/icons/batch.svg")
+                    .name("Batch")
+                    .content("Automated tasks. Offline processing of data at a time to suit you.")
+                    .build();
+            lists.add(e1);
+            lists.add(e2);
+            lists.add(e3);
+            lists.add(e4);
+            lists.add(e5);
+            lists.add(e6);
+            lists.add(e7);
+        }
+        model.addAttribute("lists",lists);
+        System.out.println(lists.toString());
+        return model.toString();
     }
 }
--- src/main/webapp/WEB-INF/ftl/test.ftl
+++ src/main/webapp/WEB-INF/ftl/test.ftl
@@ -1,1 +1,47 @@
-<h1>${u.username}</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+            integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
+            integrity="sha384-ep+dxp/oz2RKF89ALMPGc7Z89QFa32C8Uv1A3TcEK8sMzXVysblLA3+eJWTzPJzT"
+            crossorigin="anonymous"></script>
+
+    <style>
+        /*.row {*/
+        /*    display: flex;*/
+        /*    flex-wrap: wrap;*/
+        /*}*/
+        /*.row div {*/
+        /*    justify-content: center;*/
+        /*    justify-items: center;*/
+        /*}*/
+    </style>
+</head>
+<body>
+
+
+
+<div class="container marketing">
+
+    <h1 style="margin: 30px">What Spring can do</h1>
+    <div class="row">
+        <#list lists as e>
+            <div class="col-lg-2 "  style="border:1px solid #c1e2b3;margin: 10px" >
+                <img src="${e.url}" style="width: 150px;height: 150px" >
+                <h2 class="fw-normal">${e.name}</h2>
+                <p>${e.content}</p>
+            </div>
+        </#list>
+    </div>
+
+
+</div>
+</body>
+</html>
